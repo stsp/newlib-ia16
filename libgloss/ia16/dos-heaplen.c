62331c64b80fecd0e0b02f31b8ac7c7f59a96ca4
Tee-Kiah Chia <tkchia@users.noreply.github.com>
2021-10-03 05:40:44 +0000

    IA-16: -mtsr: minor tweaks

--- libgloss/ia16/dos-heaplen.c
+++ libgloss/ia16/dos-heaplen.c
@@ -5,6 +5,10 @@
 /*
  * If the linker script or the user defines a symbol __heaplen_val, use that
  * as the maximum heap size.  Otherwise, use 0.
+ *
+ * This also happens to force _heaplen to be given actual bytes in the output
+ * executable --- which is useful, because some tools might want to patch
+ * _heaplen statically (e.g. tools/ptchsize.c for FreeDOS command.com).
  */
 extern char __heaplen_val[] __attribute__ ((weak));
 size_t _heaplen = (size_t) __heaplen_val;
