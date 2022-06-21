04824acb5ff398ed11431fc6d4533cd52f5cdea5
tkchia <tkchia@users.noreply.github.com>
2022-06-21 17:39:26 +0000

    IA-16: declare _psp in <stdlib.h> if non-strict-ANSI mode

---
+++ newlib/libc/machine/ia16/machine/stdlib.h
@@ -0,0 +1,10 @@
+#ifndef	_MACHSTDLIB_H_
+#define	_MACHSTDLIB_H_
+
+#ifndef __STRICT_ANSI__
+# ifdef __MSDOS__
+extern unsigned _psp;
+# endif	/* __MSDOS__ */
+#endif 	/* !__STRICT_ANSI__ */
+
+#endif	/* _MACHSTDLIB_H_ */
